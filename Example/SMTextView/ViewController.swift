3b26c802a98e576e2fb02fc1d595c6c3e375cd0b
Spencer <mandrusiaks@users.noreply.github.com>
2017-04-24 19:50:33 -0700

    Initial commit

---
+++ Example/SMTextView/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SMTextView
+//
+//  Created by mandrusiaks on 04/24/2017.
+//  Copyright (c) 2017 mandrusiaks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
